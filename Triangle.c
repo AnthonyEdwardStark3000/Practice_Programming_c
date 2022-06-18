7847604495001baf8ecd596ffa516060b426978d
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-06-18 21:55:14 +0530

    Practice logic

---
+++ Triangle.c
@@ -0,0 +1,15 @@
+//Isosceles triangle
+#include<stdio.h>
+#include<conio.h>
+int main(){
+for(int row=0;row<=5;row+=2)
+{
+for(int col=0;col<=row;col++){
+    printf(" ");
+    printf("*");
+}
+printf("\n");
+}
+return 0;
+}
+
